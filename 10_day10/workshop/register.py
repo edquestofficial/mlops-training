0702292c3c058be0e7b68561e0d8ce7ca246b44c
Arpit Singh <arpitsingh@Arpits-MacBook-Air.local>
2025-07-15 12:21:25 +0530

    test_size updated

--- 10_day10/workshop/register.py
+++ 10_day10/workshop/register.py
@@ -11,7 +11,7 @@ df = pd.read_csv("ice_cream.csv")
 X = df[['temp']]
 y = df.iloc[:, -1]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
 
 model = pipeline.model_train(X_train, y_train)
 
